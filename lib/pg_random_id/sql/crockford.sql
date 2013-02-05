a10909b7939fae35f238a1d52ae3b1554dd6bacf
Rafał Rzepecki <divided.mind@gmail.com>
2013-02-05 18:40:09 +0100

    Check for negative numbers in crockford().

--- lib/pg_random_id/sql/crockford.sql
+++ lib/pg_random_id/sql/crockford.sql
@@ -8,6 +8,11 @@ IMMUTABLE STRICT AS $$
     charmap CONSTANT char[] = ARRAY['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
     result varchar = '';
   BEGIN
+    IF (input < 0) THEN
+      RAISE EXCEPTION numeric_value_out_of_range USING
+        MESSAGE = 'crockford(): bad input value --> ' || input,
+        HINT = 'Input needs to be nonnegative.';
+    END IF;
     LOOP
       result := charmap[(input & 31) + 1] || result;
       input := input >> 5;
