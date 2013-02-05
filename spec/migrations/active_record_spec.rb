12952e5d122851873270dba404d4077e9d0a0f3e
Rafał Rzepecki <divided.mind@gmail.com>
2013-02-05 15:25:20 +0100

    Test migration::random_id.

--- spec/migrations/active_record_spec.rb
+++ spec/migrations/active_record_spec.rb
@@ -4,11 +4,36 @@ require 'active_record'
 require 'pg_random_id/migrations/active_record'
 
 describe PgRandomId::Migrations::ActiveRecord do
+  def execute code
+    ActiveRecord::Base.connection.select_one(code)
+  end
+  
   include_context 'active_record'
   describe '#create_random_id_functions' do
     it "installs the pri_scramble function" do
       migration.create_random_id_functions
-      ActiveRecord::Base.connection.select_one("SELECT 1 FROM pg_proc WHERE proname = 'pri_scramble'").should be
+      execute("SELECT 1 FROM pg_proc WHERE proname = 'pri_scramble'").should be
+    end
+  end
+  
+  describe '#random_id' do
+    it "changes the default value" do
+      migration.create_random_id_functions
+      migration.create_table :foo
+      migration.random_id :foo
+      execute("SELECT 1 FROM pg_attrdef WHERE adrelid = 'foo'::regclass AND adsrc LIKE '%pri_scramble%'").should be
+    end
+
+    it "creates a few values without error" do
+      migration.create_random_id_functions
+      migration.create_table :foo
+      migration.random_id :foo
+      10.times do
+        expect {
+          ActiveRecord::Base.connection.execute('INSERT INTO foo VALUES(default)')
+        }.to_not raise_error
+      end
+      execute("SELECT COUNT(*) FROM foo")['count'].to_i.should == 10
     end
   end
 end
