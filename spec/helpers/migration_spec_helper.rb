88b0165d3957eb2aa58a03aa953efcaf2abf9c81
Rafał Rzepecki <divided.mind@gmail.com>
2013-02-09 22:02:07 +0100

    Refactored migration specs to accomodate Sequel.

---
+++ spec/helpers/migration_spec_helper.rb
@@ -0,0 +1,67 @@
+shared_context 'test_migration' do
+  describe '#create_random_id_functions' do
+    it "installs the functions" do
+      migration.create_random_id_functions
+      
+      execute("SELECT 1 FROM pg_proc WHERE proname = 'crockford'").should be
+      execute("SELECT 1 FROM pg_proc WHERE proname = 'pri_scramble'").should be
+    end
+  end
+  
+  describe '#random_id' do
+    it "changes the default value" do
+      migration.create_random_id_functions
+      migration.create_table :foo
+      migration.random_id :foo
+      execute("SELECT 1 FROM pg_attrdef WHERE adrelid = 'foo'::regclass AND adsrc LIKE '%pri_scramble%'").should be
+    end
+
+    it "creates a few values without error" do
+      migration.create_random_id_functions
+      migration.create_table :foo
+      migration.random_id :foo
+      10.times do
+        expect {
+          ActiveRecord::Base.connection.execute('INSERT INTO foo VALUES(default)')
+        }.to_not raise_error
+      end
+      execute("SELECT COUNT(*) FROM foo")['count'].to_i.should == 10
+    end
+  end
+
+  describe '#random_str_id' do
+    it "changes the default value" do
+      migration.create_random_id_functions
+      migration.create_table :foo
+      migration.random_str_id :foo
+      execute("SELECT 1 FROM pg_attrdef WHERE adrelid = 'foo'::regclass AND adsrc LIKE '%pri_scramble%'").should be
+      execute("SELECT 1 FROM pg_attrdef WHERE adrelid = 'foo'::regclass AND adsrc LIKE '%crockford%'").should be
+      execute("INSERT INTO foo VALUES (DEFAULT) RETURNING id;")['id'].should_not == '1'
+    end
+
+    it "changes the type" do
+      migration.create_random_id_functions
+      migration.create_table :foo
+      migration.random_str_id :foo
+      execute("""
+        SELECT typname FROM pg_attribute, pg_type 
+          WHERE attrelid = 'foo'::regclass 
+            AND attname = 'id' 
+            AND atttypid = typelem 
+            AND typname LIKE '%int%'
+      """).should_not be
+    end
+
+    it "creates a few values without error" do
+      migration.create_random_id_functions
+      migration.create_table :foo
+      migration.random_str_id :foo
+      10.times do
+        expect {
+          ActiveRecord::Base.connection.execute('INSERT INTO foo VALUES(default)')
+        }.to_not raise_error
+      end
+      execute("SELECT COUNT(*) FROM foo")['count'].to_i.should == 10
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,2 @@
 require 'helpers/active_record_helper'
+require 'helpers/migration_spec_helper'
